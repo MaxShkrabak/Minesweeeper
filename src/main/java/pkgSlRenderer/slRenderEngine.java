ef1e9873e10f071220102d73f89b5516b856b9f8
Maksim <shkrabakmaksim@gmail.com>
2024-09-23 21:53:55 -0700

    Start to work on generating a single circle

--- src/main/java/pkgSlRenderer/slRenderEngine.java
+++ src/main/java/pkgSlRenderer/slRenderEngine.java
@@ -45,8 +45,29 @@ public class slRenderEngine {
         }
     }
 
-    private void generateCircleSegmentVertices(float[] vertices, float radius, float[] center, float na, float na2, int segments) {
+    private void generateCircleSegmentVertices(float[] vertices, float radius, float[] center, float theta, float delT, int maxTriangles) {
+        for (int num_tri = 0; num_tri < maxTriangles; ++num_tri) {
+            float x1 = (float) Math.cos(theta) * radius + center[0];
+            float y1 = (float) Math.sin(theta) * radius + center[1];
 
+            theta += delT;
+
+            float x2 = (float) Math.cos(theta) * radius + center[0];
+            float y2 = (float) Math.sin(theta) * radius + center[1];
+
+            int vIndex = num_tri * 9;
+            vertices[vIndex] = center[0];
+            vertices[vIndex + 1] = center[1];
+            vertices[vIndex + 2] = 0.0f;
+
+            vertices[vIndex+ 3] = x1;
+            vertices[vIndex+ 4] = y1;
+            vertices[vIndex+ 5] = 0.0f;
+
+            vertices[vIndex + 6] = x2;
+            vertices[vIndex + 7] = y2;
+            vertices[vIndex + 8] = 0.0f;
+        }
     }
 
     private void updateRandVertices() {
@@ -54,8 +75,9 @@ public class slRenderEngine {
     }
 
     public void render() {
+        float[] circle_vertices = new float[NUM_3D_COORDS * TRIANGLES_PER_CIRCLE];
+        final float begin_angle = 0.0f, end_angle = (float) (2.0f * Math.PI) / TRIANGLES_PER_CIRCLE;
 
-        final float begin_angle = 0.0f, end_angle = (float) (2.0f * Math.PI);
         while (!my_wm.isGlfwWindowClosed()) {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);

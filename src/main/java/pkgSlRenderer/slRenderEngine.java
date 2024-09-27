ebe6ef7acfcc2ab3676d0dfeceacb7060135bfc5
Maksim <shkrabakmaksim@gmail.com>
2024-09-27 13:50:28 -0700

    Finish implementing generation of a single circle and clean up slRenderEngine code

--- src/main/java/pkgSlRenderer/slRenderEngine.java
+++ src/main/java/pkgSlRenderer/slRenderEngine.java
@@ -6,14 +6,12 @@ import pkgSlUtils.slWindowManager;
 import java.util.Random;
 
 import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11C.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11C.glClear;
 import static org.lwjgl.opengl.GL11C.glClearColor;
 
 public class slRenderEngine {
-
     private final int NUM_RGBA = 4;
     private final int NUM_3D_COORDS = 3;
     private final int TRIANGLES_PER_CIRCLE = 40;
@@ -36,62 +34,61 @@ public class slRenderEngine {
         glClearColor(CC_RED, CC_GREEN, CC_BLUE, CC_ALPHA);
 
         rand_colors = new float[MAX_CIRCLES][NUM_RGBA];
-
-        for (int i = 0; i < MAX_CIRCLES; i++) {
-            rand_colors[i][0] = rand.nextFloat();
-            rand_colors[i][1] = rand.nextFloat();
-            rand_colors[i][2] = rand.nextFloat();
-            rand_colors[i][3] = 1.0f;
-        }
+        rand_coords = new float[MAX_CIRCLES][NUM_3D_COORDS];
     }
 
-    private void generateCircleSegmentVertices(float[] vertices, float radius, float[] center, float theta, float delT, int maxTriangles) {
+    private void generateCircleSegmentVertices(float radius, float[] center, float theta, float delT, int maxTriangles) {
         for (int num_tri = 0; num_tri < maxTriangles; ++num_tri) {
+            // Calculate first vertex
             float x1 = (float) Math.cos(theta) * radius + center[0];
             float y1 = (float) Math.sin(theta) * radius + center[1];
 
             theta += delT;
 
+            // Calculate second vertex
             float x2 = (float) Math.cos(theta) * radius + center[0];
             float y2 = (float) Math.sin(theta) * radius + center[1];
 
-            int vIndex = num_tri * 9;
-            vertices[vIndex] = center[0];
-            vertices[vIndex + 1] = center[1];
-            vertices[vIndex + 2] = 0.0f;
-
-            vertices[vIndex+ 3] = x1;
-            vertices[vIndex+ 4] = y1;
-            vertices[vIndex+ 5] = 0.0f;
-
-            vertices[vIndex + 6] = x2;
-            vertices[vIndex + 7] = y2;
-            vertices[vIndex + 8] = 0.0f;
+            // Draws triangle
+            glVertex3f(center[0], center[1], 0.0f); // Center point
+            glVertex3f(x1, y1, 0.0f); // First vertex
+            glVertex3f(x2, y2, 0.0f); // Second vertex
         }
     }
 
     private void updateRandVertices() {
+        for (int i = 0; i < MAX_CIRCLES; i++) {
+            // Random colors
+            rand_colors[i][0] = rand.nextFloat(); // RED
+            rand_colors[i][1] = rand.nextFloat(); // BLUE
+            rand_colors[i][2] = rand.nextFloat(); // GREEN
+            rand_colors[i][3] = 1.0f; // ALPHA
 
+            // Random coords
+            //(to do)
+        }
     }
 
     public void render() {
-        float[] circle_vertices = new float[NUM_3D_COORDS * TRIANGLES_PER_CIRCLE];
         final float begin_angle = 0.0f, end_angle = (float) (2.0f * Math.PI) / TRIANGLES_PER_CIRCLE;
+
+        updateRandVertices(); // Updates random color
 
         while (!my_wm.isGlfwWindowClosed()) {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT);
             glBegin(GL_TRIANGLES);
 
-            glColor4f(rand_colors[0][0],rand_colors[0][1], rand_colors[0][2],rand_colors[0][3]);
-            glVertex3f(0.0f, 0.0f, 0.0f);
-            glVertex3f(0.1f, 0.0f, 0.0f);
-            glVertex3f(0.0f, 0.1f, 0.0f);
+            for (int i = 0; i < MAX_CIRCLES; i++) {
+                // Color for the circle
+                glColor4f(rand_colors[0][0], rand_colors[0][1], rand_colors[0][2], rand_colors[0][3]);
 
+                // Generates circle (CENTER)
+                generateCircleSegmentVertices(C_RADIUS, new float[]{0.0f, 0.0f}, begin_angle, end_angle, TRIANGLES_PER_CIRCLE);
+            }
             glEnd();
             my_wm.swapBuffers();
         }
         my_wm.destroyGlfwWindow();
     }
-
 }

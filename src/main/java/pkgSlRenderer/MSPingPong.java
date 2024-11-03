d0959f97d989f7bd27404f4a3dca8c3a3931b28c
Maksim <shkrabakmaksim@gmail.com>
2024-11-02 23:13:58 -0700

    Change to boolean ping-pong array and delete/modify unneeded methods and constructors

--- src/main/java/pkgSlRenderer/MSPingPong.java
+++ src/main/java/pkgSlRenderer/MSPingPong.java
@@ -3,62 +3,55 @@ package pkgSlRenderer;
 import java.util.Random;
 
 public class MSPingPong {
-    private int[][] liveArray;
-    private int[][] nextArray;
+    private boolean[][] liveArray;
+    private boolean[][] nextArray;
 
     private final int ROWS;
     private final int COLS;
     private final Random rand = new Random();
 
     public MSPingPong(int rows, int cols) {
-        this(rows, cols, 0,1); //random 0 - 1
-    }
-
-    public MSPingPong(int rows, int cols, int val) {
-        this(rows, cols, val, val);
-    }
-
-    public MSPingPong(int rows, int cols, int lowerBound, int upperBound) {
         this.ROWS = rows;
         this.COLS = cols;
-        this.liveArray = new int[rows][cols];
-        this.nextArray = new int[rows][cols];
-
-        boundResetBoard(lowerBound, upperBound);
+        this.liveArray = new boolean[rows][cols];
+        this.nextArray = new boolean[rows][cols];
+        resetBoard();
     }
 
     // Set the chosen element to a passed in value
-    private void setNext(int row, int col, int val) {
+    private void setNext(int row, int col, boolean val) {
         nextArray[row][col] = val;
     }
 
     // Gets a specific element from the Live Array
-    private int getLive(int row, int col) {
+    private boolean getLive(int row, int col) {
         return liveArray[row][col];
     }
 
     // Swap the live and next array
     public void swapArray() {
-        int[][] temp = liveArray;
+        boolean[][] temp = liveArray;
         liveArray = nextArray;
         nextArray = temp;
     }
 
     // Count the Nearest Neighbors of a cell
-    public void countNN() {
+    public int countNN() {
+        int count = 0;
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
-                int count = 0;
                 int prevC = (COLS + j - 1) % COLS;
                 int nextC = (j + 1) % COLS;
                 int nextR = (i + 1) % COLS;
                 int prevR = (ROWS + i - 1) % ROWS;
 
-                count += getLive(i, prevC) + getLive(prevR, j) + getLive(i, nextC) + getLive(nextR, j);
-
-                setNext(i, j, count);
+                count += getLive(i, prevC) ? 1 : 0;
+                count += getLive(prevR, j) ? 1 : 0;
+                count +=getLive(i, nextC) ? 1 : 0;
+                count += getLive(nextR, j) ? 1 : 0;
             }
         }
+        return count;
     }
 
     // Count the Next Nearest Neighbors of a cell
@@ -71,9 +64,30 @@ public class MSPingPong {
                 int nextR = (i + 1) % COLS;
                 int prevR = (ROWS + i - 1) % COLS;
 
-                count += getLive(nextR, prevC) + getLive(nextR, nextC) + getLive(prevR, nextC) + getLive(prevR, prevC);
+                // Nearest Neighbors
+                count += getLive(i, prevC) ? 1 : 0;
+                count += getLive(prevR, j) ? 1 : 0;
+                count += getLive(i, nextC) ? 1 : 0;
+                count += getLive(nextR, j) ? 1 : 0;
+                // Next Nearest Neighbors
+                count += getLive(nextR, prevC) ? 1 : 0;
+                count += getLive(nextR, nextC) ? 1 : 0;
+                count += getLive(prevR, nextC) ? 1 : 0;
+                count += getLive(prevR, prevC) ? 1 : 0;
 
-                setNext(i, j, count);
+                if (getLive(i, j)) {
+                    if (count == 2 || count == 3) {
+                        setNext(i, j, true);
+                    } else {
+                        setNext(i, j, false);
+                    }
+                } else {
+                    if (count == 3) {
+                        setNext(i, j, true);
+                    } else {
+                        setNext(i, j, false);
+                    }
+                }
             }
         }
     }
@@ -82,21 +96,18 @@ public class MSPingPong {
     public void printArray() {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
-                System.out.print(liveArray[i][j] + " ");
+                System.out.print((liveArray[i][j] ? 1 : 0) + " ");
             }
             System.out.println();
         }
     }
-    // Method to reset board between two values
-    public void boundResetBoard(int lower, int upper) {
-        for (int i = 0; i < liveArray.length; i++) {
-            for (int j = 0; j < liveArray[i].length; j++) {
-                liveArray[i][j] = rand.nextInt(upper - lower + 1) + lower;
-            }
-        }
-    }
+
     // Method used to reset board to 0 or 1
     public void resetBoard() {
-        boundResetBoard(0, 1);
+        for (int i = 0; i < liveArray.length; i++) {
+            for (int j = 0; j < liveArray[i].length; j++) {
+                liveArray[i][j] = rand.nextBoolean();
+            }
+        }
     }
 }

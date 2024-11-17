11625a64734fc54285386abf3956f95712636b94
Maksim <shkrabakmaksim@gmail.com>
2024-11-16 16:09:14 -0800

    Add required methods and constructors to shader and camera class

--- src/main/java/pkgSlRenderer/MSCamera.java
+++ src/main/java/pkgSlRenderer/MSCamera.java
@@ -1,4 +1,29 @@
 package pkgSlRenderer;
 
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
 public class MSCamera {
+    private Matrix4f projectionMatrix, viewMatrix;
+
+    public MSCamera() {
+        projectionMatrix = new Matrix4f();
+        viewMatrix = new Matrix4f();
+
+        projectionMatrix.identity();
+        projectionMatrix.ortho(-1f,1f,-1f,1f,0.0f,100f);
+    }
+
+    public Matrix4f getViewingMatrix() {
+        Vector3f lookFrom = new Vector3f(0.0f,0.0f,5f);
+        Vector3f cameraUp = new Vector3f(0.0f,1.0f,0.0f);
+        Vector3f lookAt = new Vector3f(0.0f,0.0f,0.0f);
+        viewMatrix.identity();
+        viewMatrix.lookAt(lookFrom, lookAt, cameraUp);
+        return viewMatrix;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
 }
--- src/main/java/pkgSlRenderer/MSShaderObject.java
+++ src/main/java/pkgSlRenderer/MSShaderObject.java
@@ -1,4 +1,44 @@
 package pkgSlRenderer;
 
+import org.joml.Matrix4f;
+import org.joml.Vector4f;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+
 public class MSShaderObject {
+
+    private int programId;
+    public MSShaderObject(String vsFile, String fsFile) {
+        programId = glCreateProgram();
+
+        int vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(programId, vsFile);
+        glCompileShader(vs);
+        glAttachShader(programId, vs);
+
+        int fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs, fsFile);
+        glCompileShader(fs);
+        glAttachShader(programId, fs);
+        glLinkProgram(programId);
+    }
+
+    public void loadMatrix4f(String strMatrixName, Matrix4f my_mat4) {
+        int var_location = glGetUniformLocation(programId, strMatrixName);
+        final int OGL_MATRIX_SIZE = 16;
+        FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(OGL_MATRIX_SIZE);
+        my_mat4.get(matrixBuffer);
+        glUniformMatrix4fv(var_location, false, matrixBuffer);
+    } // public void loadMatrix4f(...)
+
+    public void loadVector4f(String strVec4Name, Vector4f my_vec4) {
+        int var_location = glGetUniformLocation(programId, strVec4Name);
+        final int OGL_VEC4_SIZE = 4;
+        FloatBuffer vec4Buffer = BufferUtils.createFloatBuffer(OGL_VEC4_SIZE);
+        my_vec4.get(vec4Buffer);
+        glUniform4fv(var_location, vec4Buffer);
+    } // public void loadVec4f(...)
 }
